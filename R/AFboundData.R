b323621980679ef7ef02133081b3d0e12b547502
Stina Zetterstrom <stina.zetterstrom@statistik.uu.se>
2023-01-11 13:56:27 +0100

    Change input argument.

--- R/AFboundData.R
+++ R/AFboundData.R
@@ -3,7 +3,7 @@
 #' @param outcome A vector with the binary outcome variable.
 #' @param treatment A vector with the binary treatment variable.
 #' @param selection A vector with the binary selection variable or a scalar representing the probability of being selected.
-#' @param whichPar A string defining the population parameter of interest. Available options are as follows. (1) Relative risk in the total population: "RR_tot", (2) Risk difference in the total population: "RD_tot", (3) Relative risk in the subpopulation: "RR_s", (4) Risk difference in the subpopulation: "RD_s".
+#' @param whichEst A string defining the population parameter of interest. Available options are as follows. (1) Relative risk in the total population: "RR_tot", (2) Risk difference in the total population: "RD_tot", (3) Relative risk in the subpopulation: "RR_s", (4) Risk difference in the subpopulation: "RD_s".
 #'
 #' @return A scalar with the assumption free bound.
 #' @export
@@ -18,7 +18,7 @@
 #' tr = c(0,0,1,1,0,0,1,1)
 #' selProb = 0.5
 #' AFbounddata(y,tr,selProb,"RR_tot")
-AFbounddata <- function(outcome,treatment,selection,whichPar)
+AFbounddata <- function(outcome,treatment,selection,whichEst)
 {
   # A function that calculates the assumption free bound for the bias due to selection,
   # for multiple selection variables. The input is the data and which population
@@ -66,11 +66,11 @@ AFbounddata <- function(outcome,treatment,selection,whichPar)
   if( is.nan(pT0_Is1) ) stop('Input data result in 0/0. This can for instance happen if P(T=t|I_s=1)=0 or P(Y=1|T=t,I_s=1)=0.')
 
   # Calculate the assumption free bound for the relevant parameter.
-  if(whichPar=="RR_tot"){
+  if(whichEst=="RR_tot"){
     AFbound = min((pT1_Is1*pIs1+2*pIs0+pY1_T0_Is1*pT0_Is1*pIs1),1)/(pY1_T0_Is1*pT1_Is1*pIs1)
-  }else if(whichPar=="RD_tot"){
+  }else if(whichEst=="RD_tot"){
     AFbound = min((pT1_Is1*pIs1+2*pIs0+pY1_T0_Is1*pT0_Is1*pIs1),1)+pY1_T1_Is1*(1-pT1_Is1*pIs1)-pY1_T0_Is1
-  }else if(whichPar=="RR_s"){
+  }else if(whichEst=="RR_s"){
     AFbound = min((pT1_Is1+pY1_T0_Is1*pT0_Is1),1)/(pY1_T0_Is1*pT1_Is1)
   }else{
     AFbound = min((pT1_Is1+pY1_T0_Is1*pT0_Is1),1)+pY1_T1_Is1*(1-pT1_Is1)-pY1_T0_Is1
